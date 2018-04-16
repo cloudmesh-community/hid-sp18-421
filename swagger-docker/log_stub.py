e6891df9eb2e2fc930cfe28be5c6c838e178fc3e
Priya <priyadarshini.vijjigiri@gmail.com>
2018-04-16 07:38:29 -0400

    docker

--- swagger-docker/log_stub.py
+++ swagger-docker/log_stub.py
@@ -6,8 +6,6 @@ def get_log_entry():
 	    f = open('var/log/system.log')
 	    return f.readline()
 	elif platform.system() == "linux":
-		f = open(log.handlers[].stream)
-		return "Log entry:", f.readline()
-		#If log entry is emoty that means there are not any
-	elif platform.system() == "Windows":
+		return "This linux Docker environment has no logentry files"
+	else:
 		return "This linux Docker environment has no logentry files"
