2d0538c499372270c9e281a949f396c55a7fd6c7
Priya <priyadarshini.vijjigiri@gmail.com>
2018-04-16 06:56:44 -0400

    docker-swagger

--- swagger-docker/log_stub.py
+++ swagger-docker/log_stub.py
@@ -1,5 +1,10 @@
+import platform
+
 def get_log_entry():
-	file = 'var/log/debug'
-	with open(file,"r") as syslogFile:
-		log_entry = syslogFile.readline()
-	return log_entry
+	if platform.system() == "Darwin":
+		file = 'var/log/system.log'
+	    with open(file,"r") as syslogFile:
+	    	log_entry = syslogFile.readline()
+	    return log_entry
+	else:
+		return "This linux Docker environment has no logentry files"
