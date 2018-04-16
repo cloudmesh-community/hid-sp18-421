0b11798852398153e72582db032bd473585fb0e9
Priya <priyadarshini.vijjigiri@gmail.com>
2018-04-16 06:20:37 -0400

    ds

--- swagger-docker/log_stub.py
+++ swagger-docker/log_stub.py
@@ -1,11 +1,5 @@
 def get_log_entry():
-	try:
-		file = 'var/log/system.log'
-	except:
-		try:
-			file = '%SystemRoot%\System32\Winevt\Logs\Application.evtx'
-		except:
-			file = '/var/log/syslog'
+	file = 'var/log/system.log'
 	with open(file,"r") as syslogFile:
 		log_entry = syslogFile.readline()
 	return log_entry
