edad5f66d51e136558471b15e4ccc852731990c9
Priya <priyadarshini.vijjigiri@gmail.com>
2018-04-16 07:19:44 -0400

    docker-swagger

--- swagger-docker/log_stub.py
+++ swagger-docker/log_stub.py
@@ -1,12 +1,13 @@
 import platform
+import logging
 
 def get_log_entry():
 	if platform.system() == "Darwin":
-		#file = 'var/log/system.log'
 	    f = open('var/log/system.log')
-	    #log_entry = f.readline()
 	    return f.readline()
 	elif platform.system() == "linux":
-		return "This linux Docker environment has no logentry files"
+		f = open(log.handlers[0].stream)
+		return f.readline()
+		#return "This linux Docker environment has no logentry files"
 	elif platform.system() == "Windows":
 		return "This linux Docker environment has no logentry files"
